44ae72bc832838ceb6e39d856eba8ee1183f2e2f
Joe Ryan <joe@clanryan.us>
2018-04-06 22:50:39 -0400

    refactored tests to remove duplicate setup

--- hotel.rb
+++ hotel.rb
@@ -24,38 +24,37 @@ class Hotel
 end
 
 describe Hotel do
+  before do
+    @hotel = Hotel.new
+  end
+  
   describe 'checking in a guest' do
     it 'adds the guest to the hotels guest list' do
-      hotel = Hotel.new
-      hotel.check_in_guest('George Harrison', 305)
-      expect(hotel.guests).to include 'George Harrison'
+      @hotel.check_in_guest('George Harrison', 305)
+      expect(@hotel.guests).to include 'George Harrison'
     end
 
     context 'room is available' do
       it 'allows check-in' do
-        hotel = Hotel.new
-        expect(hotel.check_in_guest('George Harrison', 302)).to be true
+        expect(@hotel.check_in_guest('George Harrison', 302)).to be true
       end
     end
 
     context 'room is not available' do
       it 'disallows check-in' do
-        hotel = Hotel.new
-        hotel.check_in_guest('Roy Orbison', 302)
-        expect(hotel.check_in_guest('George Harrison', 302)).to be false
+        @hotel.check_in_guest('Roy Orbison', 302)
+        expect(@hotel.check_in_guest('George Harrison', 302)).to be false
       end
 
       it 'does not add guest to guest list' do
-
-        expect(hotel.guests).not_to include 'George Harrison'
+        expect(@hotel.guests).not_to include 'George Harrison'
       end
     end
   end
  
   it 'can check out a guest' do
-    hotel = Hotel.new
-    hotel.check_in_guest('George Orwell', 306)
-    hotel.check_out_guest('George Orwell')
-    expect(hotel.guests).not_to include 'George Orwell'
+    @hotel.check_in_guest('George Orwell', 306)
+    @hotel.check_out_guest('George Orwell')
+    expect(@hotel.guests).not_to include 'George Orwell'
   end
 end
